84254eb7273abc015cd650ebc93b0ce267bb4238
Demon000 <demonsingur@gmail.com>
2020-10-31 16:28:38 +0200

    server: remove hello world

--- server/api/v1/api_v1.py
+++ server/api/v1/api_v1.py
@@ -6,7 +6,6 @@ api = Blueprint('api_v1', __name__)
 @api.route('/about')
 def api_v1_about():
     return jsonify({
-        'hello': 'world',
         'description': 'Version 1 API endpoint',
         'version': 1.0,
     })

05d5d5e2ba301e4a61833640ec789930678529c8
andrei <zaicescuandrei@gmail.com>
2020-10-31 18:13:26 +0200

    web app

---
+++ web-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material-module';
+import { LoginFormComponent } from './components/shared/login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

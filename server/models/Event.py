bfc5ef437d27167d146b1904197c15ab4fc93e75
Demon000 <demonsingur@gmail.com>
2020-11-16 14:29:25 +0200

    server: add max and current number of invitations

--- server/models/Event.py
+++ server/models/Event.py
@@ -45,13 +45,15 @@ class Event(Document):
     location = StringField()
     location_point = PointField()
     date = StringField(required=True)
+    no_participants = IntField(min_value=0, default=0)
+    no_max_participants = IntField(min_value=0, default=0)
     description = StringField(required=True)
     visibility = IntField(min_value=event_visibility_map.minimum_key(),
                           max_value=event_visibility_map.maximum_key(), required=True)
     category = IntField(min_value=event_category_map.minimum_key(),
                         max_value=event_category_map.maximum_key(), required=True)
 
-    def to_dict(self, hide_details: bool = False):
+    def to_dict(self, with_details: bool = False):
         #
         # HACK: location point is list when the object was created, but gets saved
         # as a dict with a coordinates key containing the list
@@ -73,17 +75,26 @@ class Event(Document):
             'visibility_text': event_visibility_map.to_value(self.visibility),
             'category': self.category,
             'category_text': event_category_map.to_value(self.category),
-            'is_limited_details': hide_details,
+            'is_limited_details': with_details,
+            'is_unlimited_participants': self.no_max_participants == 0,
         }
 
-        if hide_details:
+        if with_details:
+            d['location'] = self.location
+            d['location_points'] = location_points
+            d['location_points_radius_meters'] = None
+            d['no_participants'] = self.no_participants
+            d['no_max_participants'] = self.no_max_participants
+            d['is_unlimited_participants'] = self.no_max_participants == 0
+            d['allows_more_participants'] = self.no_participants < self.no_max_participants
+        else:
             approximate, radius_meters = approximate_location(location_points)
             d['location'] = None
             d['location_points'] = approximate
             d['location_points_radius_meters'] = radius_meters
-        else:
-            d['location'] = self.location
-            d['location_points'] = location_points
-            d['location_points_radius_meters'] = None
+            d['no_participants'] = None
+            d['no_max_participants'] = None
+            d['is_unlimited_participants'] = None
+            d['allows_more_participants'] = None
 
         return d
--- server/services/EventInvitationService.py
+++ server/services/EventInvitationService.py
@@ -2,31 +2,36 @@ from typing import Union
 
 from mongoengine import DoesNotExist
 
-from models.Event import Event
+from models.Event import Event, event_visibility_map, EVENT_VISIBILITY_WHITELIST, EVENT_VISIBILITY_PUBLIC
 from models.EventInvitation import EVENT_INVITATION_STATUS_PENDING, EVENT_INVITATION_STATUS_ACCEPTED, \
     EventInvitation, event_invitation_status_map
 
 from models.User import User
-from utils.errors import EventInvitationAlreadyExists
+from utils.errors import EventInvitationAlreadyExists, EventInvitationCannotJoinPrivate
 
 
 class EventInvitationService:
-    def add(self, event: Event, user: User, accepted: bool = False):
+    def join(self, event: Event, user: User):
         try:
             EventInvitation.objects.get(user=user, event=event)
             raise EventInvitationAlreadyExists()
         except DoesNotExist:
             pass
 
-        status = event_invitation_status_map.to_key(EVENT_INVITATION_STATUS_ACCEPTED) if accepted \
-            else event_invitation_status_map.to_key(EVENT_INVITATION_STATUS_PENDING)
-        event_invitation = EventInvitation(event=event, user=user, status=status)
+        event_invitation = EventInvitation(event=event, user=user)
         event_invitation.save()
+
+        if event.visibility == event_visibility_map.to_key(EVENT_VISIBILITY_PUBLIC):
+            self.accept(event_invitation)
+
         return event_invitation
 
     def accept(self, event_invitation: EventInvitation):
         event_invitation.status = event_invitation_status_map.to_key(EVENT_INVITATION_STATUS_ACCEPTED)
         event_invitation.save()
+
+        event_invitation.event.no_participants += 1
+        event_invitation.event.save()
 
     def find_one_by(self, *args, **kwargs) -> Union[EventInvitation, None]:
         try:
@@ -42,7 +47,6 @@ class EventInvitationService:
             return self.find_by()
         else:
             return self.find_by(status=event_invitation_status_map.to_key(EVENT_INVITATION_STATUS_ACCEPTED))
-
 
     def find_accepted_user_invitations_event_ids(self, user: User):
         accepted_event_invitations = self.find_by(

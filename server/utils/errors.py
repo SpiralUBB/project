f178320415eafe951ed5285284e60f404e998501
Demon000 <demonsingur@gmail.com>
2020-11-30 01:54:23 +0200

    server: consistently indent errors

--- server/utils/errors.py
+++ server/utils/errors.py
@@ -23,59 +23,83 @@ def HttpErrorFactory(name, code, status, default_message):
     })
 
 
-UserAlreadyExists = HttpErrorFactory('UserAlreadyExists', 'user-already-exists', 403,
+UserAlreadyExists = HttpErrorFactory('UserAlreadyExists',
+                                     'user-already-exists', 403,
                                      'User already exists')
-UserDoesNotExist = HttpErrorFactory('UserDoesNotExist', 'user-not-exist', 404,
+UserDoesNotExist = HttpErrorFactory('UserDoesNotExist',
+                                    'user-not-exist', 404,
                                     'User does not exist')
-UserUsernameInvalid = HttpErrorFactory('UserUsernameInvalid', 'user-username-invalid', 400,
+UserUsernameInvalid = HttpErrorFactory('UserUsernameInvalid',
+                                       'user-username-invalid', 400,
                                        'User username is invalid')
-UserPasswordInvalid = HttpErrorFactory('UserPasswordInvalid', 'user-password-invalid', 400,
+UserPasswordInvalid = HttpErrorFactory('UserPasswordInvalid',
+                                       'user-password-invalid', 400,
                                        'User password is invalid')
-UserFirstNameInvalid = HttpErrorFactory('UserFirstNameInvalid', 'user-first-name-invalid', 400,
+UserFirstNameInvalid = HttpErrorFactory('UserFirstNameInvalid',
+                                        'user-first-name-invalid', 400,
                                         'User first name is invalid')
-UserLastNameInvalid = HttpErrorFactory('UserLastNameInvalid', 'user-last-name-invalid', 400,
+UserLastNameInvalid = HttpErrorFactory('UserLastNameInvalid',
+                                       'user-last-name-invalid', 400,
                                        'User last name is invalid')
-UserLoginFailed = HttpErrorFactory('UserLoginFailed', 'user-login-failed', 401,
+UserLoginFailed = HttpErrorFactory('UserLoginFailed',
+                                   'user-login-failed', 401,
                                    'User login failed')
-UserTokenExpired = HttpErrorFactory('UserTokenExpired', 'user-token-expired', 401,
+UserTokenExpired = HttpErrorFactory('UserTokenExpired',
+                                    'user-token-expired', 401,
                                     'User token expired')
-UserTokenInvalid = HttpErrorFactory('UserTokenInvalid', 'user-token-invalid', 422,
+UserTokenInvalid = HttpErrorFactory('UserTokenInvalid',
+                                    'user-token-invalid', 422,
                                     'User token is invalid')
-EventDoesNotExist = HttpErrorFactory('EventDoesNotExist', 'event-not-exist', 404,
+EventDoesNotExist = HttpErrorFactory('EventDoesNotExist',
+                                     'event-not-exist', 404,
                                      'Event does not exist')
-EventOwnerInvalid = HttpErrorFactory('EventOwnerInvalid', 'event-owner-invalid', 400,
+EventOwnerInvalid = HttpErrorFactory('EventOwnerInvalid',
+                                     'event-owner-invalid', 400,
                                      'Event owner is invalid')
-EventTitleInvalid = HttpErrorFactory('EventTitleInvalid', 'event-title-invalid', 400,
+EventTitleInvalid = HttpErrorFactory('EventTitleInvalid',
+                                     'event-title-invalid', 400,
                                      'Event title is invalid')
-EventLocationInvalid = HttpErrorFactory('EventLocationInvalid', 'event-location-invalid', 400,
+EventLocationInvalid = HttpErrorFactory('EventLocationInvalid',
+                                        'event-location-invalid', 400,
                                         'Event location is invalid')
-EventLocationPointInvalid = HttpErrorFactory('EventLocationPointInvalid', 'event-location-point-invalid', 400,
+EventLocationPointInvalid = HttpErrorFactory('EventLocationPointInvalid',
+                                             'event-location-point-invalid', 400,
                                              'Event location point is invalid')
-EventTimeInvalid = HttpErrorFactory('EventTimeInvalid', 'event-time-invalid', 400,
+EventTimeInvalid = HttpErrorFactory('EventTimeInvalid',
+                                    'event-time-invalid', 400,
                                     'Event time is invalid')
 EventMaxNoParticipantsInvalid = HttpErrorFactory('EventMaxNoParticipantsInvalid',
                                                  'event-max-no-participants-invalid', 400,
                                                  'Event max number of participants is invalid')
-EventDescriptionInvalid = HttpErrorFactory('EventDescriptionInvalid', 'event-description-invalid', 400,
+EventDescriptionInvalid = HttpErrorFactory('EventDescriptionInvalid',
+                                           'event-description-invalid', 400,
                                            'Event description is invalid')
-EventVisibilityInvalid = HttpErrorFactory('EventVisibilityInvalid', 'event-visibility-invalid', 400,
+EventVisibilityInvalid = HttpErrorFactory('EventVisibilityInvalid',
+                                          'event-visibility-invalid', 400,
                                           'Event visibility type is invalid')
-EventCategoryInvalid = HttpErrorFactory('EventCategoryInvalid', 'event-category-invalid', 400,
+EventCategoryInvalid = HttpErrorFactory('EventCategoryInvalid',
+                                        'event-category-invalid', 400,
                                         'Event category is invalid')
-EventCommentDoesNotExist = HttpErrorFactory('EventCommentDoesNotExist', 'event-comment-not-exist', 404,
+EventCommentDoesNotExist = HttpErrorFactory('EventCommentDoesNotExist',
+                                            'event-comment-not-exist', 404,
                                             'Event comment does not exist')
-EventCommentAuthorInvalid = HttpErrorFactory('EventCommentAuthorInvalid', 'event-comment-author-invalid', 400,
+EventCommentAuthorInvalid = HttpErrorFactory('EventCommentAuthorInvalid',
+                                             'event-comment-author-invalid', 400,
                                              'Event comment author invalid')
-EventInvitationAlreadyExists = HttpErrorFactory('EventInvitationAlreadyExists', 'event-invitation-already-exists', 403,
+EventInvitationAlreadyExists = HttpErrorFactory('EventInvitationAlreadyExists',
+                                                'event-invitation-already-exists', 403,
                                                 'Event invitation already exists')
-EventInvitationDoesNotExist = HttpErrorFactory('EventInvitationDoesNotExist', 'event-invitation-not-exist', 404,
+EventInvitationDoesNotExist = HttpErrorFactory('EventInvitationDoesNotExist',
+                                               'event-invitation-not-exist', 404,
                                                'Event invitation does not exist')
-EventInvitationStatusInvalid = HttpErrorFactory('EventInvitationStatusInvalid', 'event-invitation-status-invalid', 400,
+EventInvitationStatusInvalid = HttpErrorFactory('EventInvitationStatusInvalid',
+                                                'event-invitation-status-invalid', 400,
                                                 'Event invitation status is invalid')
 EventInvitationAttendStatusInvalid = HttpErrorFactory('EventInvitationAttendStatusInvalid',
                                                       'event-invitation-attend-status-invalid', 400,
                                                       'Event invitation attend status is invalid')
-EventInvitationCannotJoinOwn = HttpErrorFactory('EventInvitationCannotJoinOwn', 'event-invitation-cannot-join-own', 400,
+EventInvitationCannotJoinOwn = HttpErrorFactory('EventInvitationCannotJoinOwn',
+                                                'event-invitation-cannot-join-own', 400,
                                                 'Cannot join an event owned by yourself')
 EventInvitationCannotJoinFull = HttpErrorFactory('EventInvitationCannotJoinFull',
                                                  'event-invitation-cannot-join-full', 400,
@@ -83,19 +107,27 @@ EventInvitationCannotJoinFull = HttpErrorFactory('EventInvitationCannotJoinFull'
 EventInvitationCannotModifyOwn = HttpErrorFactory('EventInvitationCannotModifyOwn',
                                                   'event-invitation-cannot-modify own', 400,
                                                   'Cannot modify invitation for own event')
-EventCommentEventInvalid = HttpErrorFactory('EventCommentEventInvalid', 'event-comment-event-invalid', 400,
+EventCommentEventInvalid = HttpErrorFactory('EventCommentEventInvalid',
+                                            'event-comment-event-invalid', 400,
                                             'Event comment event invalid')
-EventCommentTextInvalid = HttpErrorFactory('EventCommentTextInvalid', 'event-comment-text-invalid', 400,
+EventCommentTextInvalid = HttpErrorFactory('EventCommentTextInvalid',
+                                           'event-comment-text-invalid', 400,
                                            'Event comment text invalid')
-UserFeedbackAlreadyExists = HttpErrorFactory('UserFeedbackAlreadyExists', 'user-feedback-already-exists', 400,
+UserFeedbackAlreadyExists = HttpErrorFactory('UserFeedbackAlreadyExists',
+                                             'user-feedback-already-exists', 400,
                                              'User feedback already exists')
-UserFeedbackDoesNotExist = HttpErrorFactory('UserFeedbackDoesNotExist', 'user-feedback-not-exist', 400,
+UserFeedbackDoesNotExist = HttpErrorFactory('UserFeedbackDoesNotExist',
+                                            'user-feedback-not-exist', 400,
                                             'User feedback does not exist')
-UserFeedbackPointsInvalid = HttpErrorFactory('UserFeedbackPointsInvalid', 'user-feedback-points-invalid', 400,
+UserFeedbackPointsInvalid = HttpErrorFactory('UserFeedbackPointsInvalid',
+                                             'user-feedback-points-invalid', 400,
                                              'User feedback points invalid')
-UserFeedbackCannotGiveOwn = HttpErrorFactory('UserFeedbackCannotGiveOwn', 'user-feedback-cannot-give-own', 400,
+UserFeedbackCannotGiveOwn = HttpErrorFactory('UserFeedbackCannotGiveOwn',
+                                             'user-feedback-cannot-give-own', 400,
                                              'Cannot give user feedback to yourself')
-PaginationLimitInvalid = HttpErrorFactory('PaginationLimitInvalid', 'pagination-limit-invalid', 400,
+PaginationLimitInvalid = HttpErrorFactory('PaginationLimitInvalid',
+                                          'pagination-limit-invalid', 400,
                                           'Pagination limit invalid')
-PaginationPageInvalid = HttpErrorFactory('PaginationPageInvalid', 'pagination-page-invalid', 400,
+PaginationPageInvalid = HttpErrorFactory('PaginationPageInvalid',
+                                         'pagination-page-invalid', 400,
                                          'Pagination page invalid')

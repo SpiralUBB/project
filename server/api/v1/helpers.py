9726e055112bb3b7b4800373a5d97678ad7e43aa
Demon000 <demonsingur@gmail.com>
2020-12-05 21:15:46 +0200

    server: upload missing helpers file

---
+++ server/api/v1/helpers.py
@@ -0,0 +1,38 @@
+from functools import wraps
+
+from flask import request
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request_optional, verify_jwt_in_request
+
+from services.UserService import UserService
+from utils.dependencies import services_injector
+from utils.errors import UserNotLoggedIn, UserLoggedInInvalid
+
+
+def retrieve_logged_in_user(optional=False):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            request.user = None
+
+            user_service = services_injector.get(UserService)
+            if optional:
+                verify_jwt_in_request_optional()
+            else:
+                verify_jwt_in_request()
+
+            username = get_jwt_identity()
+            if not username:
+                if optional:
+                    return
+                else:
+                    raise UserNotLoggedIn()
+
+            request.user = user_service.find_one_by(username=username)
+            if not request.user:
+                raise UserLoggedInInvalid()
+
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return decorator

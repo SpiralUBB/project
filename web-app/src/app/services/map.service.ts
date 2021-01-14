71270fbff0cd4704929395d8d61b061b90d6eeec
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2021-01-15 00:10:51 +0200

    Map marker with multiple events

--- web-app/src/app/services/map.service.ts
+++ web-app/src/app/services/map.service.ts
@@ -52,24 +52,39 @@ export class MapService {
         this.events.push(eventsRes.items[key]);
       });
       console.log(this.events);
-      this.eventsLayer = Object.values(this.events).map((event) =>
-        marker(latLng(event.locationPoints[0], event.locationPoints[1]), {
+
+      const locationEvents = new Map();
+      Object.values(this.events).forEach((event) => {
+        const location = String(event.locationPoints[0]) + "," + String(event.locationPoints[1]);
+
+        if (!locationEvents.has(location)) {
+          locationEvents.set(location, []);
+        }
+
+        locationEvents.set(location, [...locationEvents.get(location), event]);
+      })
+
+      this.eventsLayer = [];
+      locationEvents.forEach((events, locationPoints) => {
+        const m = marker(latLng(events[0].locationPoints[0], events[0].locationPoints[1]), {
           icon: icon({
             iconSize: [25, 41],
             iconAnchor: [13, 41],
             iconUrl: 'leaflet/marker-icon.png',
             shadowUrl: 'leaflet/marker-shadow.png',
           }),
-        }).on('click', (e) => this.openEventCard(event))
-      );
+        }).on('click', (e) => this.openEventCard(events));
+
+        this.eventsLayer.push(m);
+      });
     });
   }
 
-  openEventCard(event: AppEvent) {
+  openEventCard(events: AppEvent[]) {
     this.ngZone.run(() => {
       this.dialog.open(MarkerPopupComponent, {
         maxWidth: '100vw !important',
-        data: { event },
+        data: { events },
       });
     });
   }

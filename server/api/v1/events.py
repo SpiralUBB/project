02dda5ccce55192d5e610c2bae9df722b65b125e
Demon000 <demonsingur@gmail.com>
2021-01-15 12:27:21 +0200

    server: fix filter_exclude_owner

--- server/api/v1/events.py
+++ server/api/v1/events.py
@@ -108,7 +108,7 @@ def events_get(event_service: EventService, event_invitation_service: EventInvit
     events = event_service.find_visible_for_user(user, invited_event_ids, show_public=True, show_whitelist=True,
                                                  categories=categories, date_start=date_start, date_end=date_end,
                                                  filter_ids=filter_event_ids, filter_owner=filter_owner,
-                                                 filter_exclude_owner=exclude_owner)
+                                                 filter_exclude_owner=filter_exclude_owner)
 
     full_details_event_ids = event_invitation_service.find_accepted_user_invitations_event_ids(user)
     return jsonify(get_paginated_items_from_qs(events, event_to_restricted_dict, user, full_details_event_ids))

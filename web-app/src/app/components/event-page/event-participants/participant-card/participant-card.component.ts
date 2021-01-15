04ce249d2912753e1a9c970dc05d32a31d4bf1bb
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2021-01-15 16:28:04 +0200

    event participants

--- web-app/src/app/components/event-page/event-participants/participant-card/participant-card.component.ts
+++ web-app/src/app/components/event-page/event-participants/participant-card/participant-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/app/models/user';
+import { ParticipantFeedbackComponent } from '../participant-feedback/participant-feedback.component';
 
 @Component({
   selector: 'app-participant-card',
@@ -9,7 +11,17 @@ import { User } from 'src/app/models/user';
 export class ParticipantCardComponent implements OnInit {
   @Input() user: User;
   @Input() canLeaveFeedback = true;
-  constructor() {}
+  constructor(
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
+  
+  sendFeedback() {
+    this.dialog.open(ParticipantFeedbackComponent, {
+      data: {
+        user: this.user 
+      }
+    })
+  }
 }

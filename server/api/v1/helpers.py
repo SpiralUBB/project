055c6244e97a0541bf6bd38446ad5fde4e539b0f
Demon000 <demonsingur@gmail.com>
2021-01-15 11:09:56 +0200

    server: automatically update access token if needed

--- server/api/v1/helpers.py
+++ server/api/v1/helpers.py
@@ -2,8 +2,10 @@ from enum import Enum
 from functools import wraps
 from typing import Union
 
-from flask import request
-from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request_optional, verify_jwt_in_request
+from flask import request, Response
+from flask_jwt_extended import get_jwt_identity, \
+    create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies, unset_refresh_cookies, \
+    unset_access_cookies, verify_jwt_refresh_token_in_request, verify_fresh_jwt_in_request
 
 from models.User import User
 from services.EventCommentService import EventCommentService
@@ -15,16 +17,61 @@ from utils.errors import UserNotLoggedIn, UserLoggedInInvalid, EventDoesNotExist
     EventInvitationDoesNotExist
 
 
+def _create_fresh_access_token(username: str):
+    return create_access_token(identity=username, fresh=True)
+
+
+def _create_refresh_token(username: str):
+    return create_refresh_token(identity=username)
+
+
+def set_access_token(response: Response, access_token):
+    set_access_cookies(response, access_token)
+
+
+def set_refresh_token(response: Response, refresh_token):
+    set_refresh_cookies(response, refresh_token)
+
+
+def set_new_tokens(response: Response, username: str):
+    access_token = _create_fresh_access_token(username)
+    set_access_token(response, access_token)
+    refresh_token = _create_refresh_token(username)
+    set_refresh_token(response, refresh_token)
+    return access_token, refresh_token
+
+
+def unset_tokens(response: Response):
+    unset_access_cookies(response)
+    unset_refresh_cookies(response)
+
+
+def verify_access_token(optional=False):
+    try:
+        verify_jwt_refresh_token_in_request()
+        username = get_jwt_identity()
+    except Exception as e:
+        if optional:
+            return
+        else:
+            raise UserNotLoggedIn(original_message=str(e))
+
+    try:
+        verify_fresh_jwt_in_request()
+        return None
+    except Exception:
+        pass
+
+    return _create_fresh_access_token(username)
+
+
 def retrieve_logged_in_user(optional=False):
     def decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
             request.user = None
 
-            if optional:
-                verify_jwt_in_request_optional()
-            else:
-                verify_jwt_in_request()
+            access_token = verify_access_token(optional)
 
             username = get_jwt_identity()
             if not username and not optional:
@@ -40,7 +87,12 @@ def retrieve_logged_in_user(optional=False):
 
             request.user = user
 
-            return fn(*args, **kwargs)
+            response = fn(*args, **kwargs)
+
+            if access_token is not None:
+                set_access_token(response, access_token)
+
+            return response
 
         return wrapper
 

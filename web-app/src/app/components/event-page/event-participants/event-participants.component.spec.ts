abbdb3b3d55ace3157e80ff5ffa5d105e6f466c2
Demon000 <demonsingur@gmail.com>
2021-01-17 23:11:53 +0200

    Revert "web-app: rename participants card"
    This reverts commit 4d1d78b61d082f09bc2f0b1983aa6ed1eea4885d.

--- web-app/src/app/components/event-page/event-participants/event-participants.component.spec.ts
+++ web-app/src/app/components/event-page/event-participants/event-participants.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EventParticipantsSimpleComponent } from './event-participants-simple.component';
+import { EventParticipantsComponent } from './event-participants.component';
 
 describe('EventParticipantsComponent', () => {
-  let component: EventParticipantsSimpleComponent;
-  let fixture: ComponentFixture<EventParticipantsSimpleComponent>;
+  let component: EventParticipantsComponent;
+  let fixture: ComponentFixture<EventParticipantsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EventParticipantsSimpleComponent],
+      declarations: [EventParticipantsComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EventParticipantsSimpleComponent);
+    fixture = TestBed.createComponent(EventParticipantsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
